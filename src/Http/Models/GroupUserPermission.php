648f4d2db03d4b5851c23509d70cef88afd0ea7a
蒋昱 <149087821@qq.com>
2020-07-25 11:45:35 +0800

    no message

---
+++ src/Http/Models/GroupUserPermission.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Zijinghua\Zvoyager\Http\Models;
+
+
+use Zijinghua\Zbasement\Http\Models\BaseModel;
+use Zijinghua\Zvoyager\Http\Contracts\GroupPermissionModelInterface;
+
+class GroupUserPermission extends BaseModel implements GroupPermissionModelInterface
+{
+    protected $table='group_user_permissions';
+}
