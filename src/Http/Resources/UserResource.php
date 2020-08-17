574c71277e53f995b1447b4fdffc406d9bd4ffd2
蒋昱 <149087821@qq.com>
2020-08-17 10:21:52 +0800

    no message

--- src/Http/Resources/UserResource.php
+++ src/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'uuid'=>@$this->resource['uuid'],
+            'id'=>@$this->resource['id'],
             'username'=>@$this->resource['username'],
             'email'=>@$this->resource['email'],
             'mobile'=>@$this->resource['mobile'],
